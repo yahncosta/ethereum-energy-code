5cf26a3b02cb14a6659c8e8496e7a3b8711abe7b
Yahn <y.costahackspacher@stud.uni-goettingen.de>
2026-05-25 18:40:10 +0000

    web3pi client correction

--- inference/inference/web3pi2024.py
+++ inference/inference/web3pi2024.py
@@ -3,6 +3,7 @@ import pandas as pd
 from inference.constants.p2p_spec2020 import GOSSIP_PHASE_SATURATED
 from inference.constants.web3pi2024 import (
     WEB3PI_CONSENSUS_CLIENT,
+    WEB3PI_EXECUTION_CLIENT,
     WEB3PI_HW_ARCH,
     WEB3PI_POWER_W,
 )
@@ -14,6 +15,7 @@ def infer_web3pi_features(df: pd.DataFrame) -> pd.DataFrame:
     web3pi_mask = (
         (df["hw_arch"] == WEB3PI_HW_ARCH)
         & (df["consensus_client"] == WEB3PI_CONSENSUS_CLIENT)
+        & (df["execution_client"] == WEB3PI_EXECUTION_CLIENT)
         & (df["cloud_provider"].isna())
         & (df["gossip_phase"] == GOSSIP_PHASE_SATURATED)
     )
